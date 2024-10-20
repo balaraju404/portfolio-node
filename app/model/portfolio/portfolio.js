8ee6355f8d48215992c0ac398c4f6d84796e0c80
balaraju <balarajugandham1@gmail.com>
2024-10-21 00:28:51 +0530

    filter condition updated

--- app/model/portfolio/portfolio.js
+++ app/model/portfolio/portfolio.js
@@ -1,4 +1,5 @@
 const { getDb } = require('../../db-conn/db-conn');
+const { ObjectId } = require('mongodb');
 
 exports.create = async (reqParams) => {
  try {
@@ -30,7 +31,7 @@ exports.create = async (reqParams) => {
 
   return { status: true, msg: 'Portfolio Created Successfully', insertedId: result.insertedId };
  } catch (error) {
-  return { status: false, msg: error.message || 'An error occurred' }; // Consistent error handling
+  return { status: false, msg: error.message || 'An error occurred' }; 
  }
 };
 
@@ -40,32 +41,28 @@ exports.details = async (reqParams) => {
   const portfolio_name = reqParams['portfolio_name'] || ''
   const portfolio_id = reqParams['portfolio_id'] || ''
 
-
   const query = {};
-
   if (userId) {
-   query.user_id = userId; // Directly assign userId if it exists
+   query.user_id = userId;
   }
-
   if (portfolio_name) {
-   query.portfolio_name = portfolio_name; // Directly assign portfolio_name if it exists
+   query.portfolio_name = portfolio_name;
   }
-
   if (portfolio_id) {
-   query._id = portfolio_id; // Directly assign portfolio_id if it exists
+   query._id = new ObjectId(portfolio_id);
   }
 
-  const db = getDb();
+  const db = getDb();  
   const collection = db.collection(TBL_PORTFOLIOS);
-  const result = await collection.find(query).sort({ login_name: 1 }).toArray();
+  const result = await collection.find(query).sort({ portfolio_name: 1 }).toArray();
 
   result.forEach(e => {
-   e.portfolio_id = e._id; // Consider using a different field name to avoid confusion with the original
+   e.portfolio_id = e._id;
   });
 
   return { status: true, data: result };
  } catch (error) {
-  return { status: false, msg: error.message || 'An error occurred' }; // Consistent error handling
+  return { status: false, msg: error.message || 'An error occurred' };
  }
 };
 
